b2902bf497ed1f11068f0fa62dd4f6f227373c15
KABA N'faly <nfalykaba@gmail.com>
2015-07-14 20:27:14 +0200

    css integration

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,8 +1,17 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="UTF-8">
-        <title><?php echo $title ; ?></title>
-    </head>
-    <body>
-   
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title><?php echo $title; ?></title>
+        <link rel="stylesheet" type="text/css" media="all" href="<?php echo base_url(); ?>css/style.css" >      
+    </head>    
+</head>
+<body>
+    <header class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="heading">
+            </div>
+        </div>
+    </header>
