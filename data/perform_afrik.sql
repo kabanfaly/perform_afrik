ac54b633403c97b8b70b75d4cfb6b9f9b5a79eb9
KABA N'faly <nfalykaba@gmail.com>
2015-08-03 00:05:25 +0200

    Download data

--- data/perform_afrik.sql
+++ data/perform_afrik.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client: localhost
--- Généré le : Mer 29 Juillet 2015 à 23:39
+-- Généré le : Lun 03 Août 2015 à 00:04
 -- Version du serveur: 5.5.44
 -- Version de PHP: 5.3.10-1ubuntu3.19
 
@@ -47,7 +47,33 @@ CREATE TABLE IF NOT EXISTS `pa_camion` (
   `id_camion` int(11) NOT NULL AUTO_INCREMENT,
   `numero` varchar(45) NOT NULL,
   PRIMARY KEY (`id_camion`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=35 ;
+
+--
+-- Contenu de la table `pa_camion`
+--
+
+INSERT INTO `pa_camion` (`id_camion`, `numero`) VALUES
+(1, '1209GJ01/1645FX01'),
+(13, '3159FN01/2440ES02'),
+(16, '6654FB01'),
+(17, '3266EL04/1227AV01'),
+(19, '5837GC01/2908GA01'),
+(20, '730GH02/412EJ02'),
+(21, '1249FJ01/6107EE02'),
+(22, '8969EA02/8970EA02'),
+(23, '5501PL02/8182FL02'),
+(24, '1890FL02/7915AY06'),
+(25, '1208GJ01/4143AZ01'),
+(26, '4894GJL01/2792GN01'),
+(27, '5450CK01/4893CR01'),
+(28, '3746EB01/5217CJ01'),
+(29, '721EH01/2130BH04'),
+(30, '7716FP03/7852EH01'),
+(31, '7399CT03/4183AR01'),
+(32, '3459FH01/8028CG06'),
+(33, '9905FH03/9896FH03'),
+(34, '972FP02/');
 
 -- --------------------------------------------------------
 
@@ -57,7 +83,7 @@ CREATE TABLE IF NOT EXISTS `pa_camion` (
 
 DROP TABLE IF EXISTS `pa_dechargement`;
 CREATE TABLE IF NOT EXISTS `pa_dechargement` (
-`id_dechargement` int(11) NOT NULL AUTO_INCREMENT,
+  `id_dechargement` int(11) NOT NULL AUTO_INCREMENT,
   `id_camion` int(11) NOT NULL,
   `id_ville` int(11) NOT NULL,
   `id_fournisseur` int(11) NOT NULL,
@@ -74,7 +100,7 @@ CREATE TABLE IF NOT EXISTS `pa_dechargement` (
   KEY `id_camion_2` (`id_camion`),
   KEY `id_fournisseur` (`id_fournisseur`),
   KEY `id_ville` (`id_ville`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 -- --------------------------------------------------------
 
@@ -89,7 +115,25 @@ CREATE TABLE IF NOT EXISTS `pa_fournisseur` (
   `telephone` varchar(45) DEFAULT NULL,
   `adresse` text,
   PRIMARY KEY (`id_fournisseur`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=15 ;
+
+--
+-- Contenu de la table `pa_fournisseur`
+--
+
+INSERT INTO `pa_fournisseur` (`id_fournisseur`, `nom`, `telephone`, `adresse`) VALUES
+(1, 'Kanté   Fatoumata', NULL, NULL),
+(2, 'Coulibaly Oumar', NULL, NULL),
+(3, 'Kone Ismael', NULL, NULL),
+(6, 'Yeo', '', ''),
+(7, 'Bassole', '', ''),
+(8, 'Meite Hamidou', '', ''),
+(9, 'Dabo Ibrahim', '', ''),
+(10, 'Diaby Hamadou', '', ''),
+(11, 'Traore Soumaila', '', ''),
+(12, 'Diakite Idrissa', '', ''),
+(13, 'Laye Amara', '', ''),
+(14, 'Pele', '', '');
 
 -- --------------------------------------------------------
 
@@ -102,17 +146,26 @@ CREATE TABLE IF NOT EXISTS `pa_ville` (
   `id_ville` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(45) NOT NULL,
   PRIMARY KEY (`id_ville`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=21 ;
 
 --
 -- Contenu de la table `pa_ville`
 --
 
 INSERT INTO `pa_ville` (`id_ville`, `nom`) VALUES
-(1, 'Abidjan'),
-(2, 'Yamoussokoro'),
-(3, 'Man'),
-(4, 'Daloa');
+(8, 'Seguela'),
+(9, 'Gohitafla'),
+(10, 'Beoumi'),
+(11, 'Dianra'),
+(12, 'Mankono'),
+(13, 'M''bahiakro'),
+(14, 'Kani'),
+(15, 'Zoukougbe'),
+(16, 'Kounayiiri'),
+(17, 'Vavoua'),
+(18, 'Tiebissou'),
+(19, 'Dabakala'),
+(20, 'Odienne');
 
 --
 -- Contraintes pour les tables exportées
