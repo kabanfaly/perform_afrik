9ad8c5bcece4ad02206ec41ce1c99e3c419f6f48
KABA N'faly <nfalykaba@gmail.com>
2015-08-03 00:41:06 +0200

    Cleaning

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -7,15 +7,6 @@
         <link rel="stylesheet" type="text/css" media="all" href="<?php echo base_url(); ?>css/style.css" >      
     </head>
     <body>
-        <!--        <header class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-                    <div class="container-fluid">
-                        <div class="heading">
-                            <a href="<?php echo base_url(); ?>">
-                                <img width="120" height="40" border="0" title="Performe Afrik" alt="LOGO" src="<?php echo base_url(); ?>images/logo.png" />
-                            </a>
-                        </div>
-                    </div>
-                </header>-->
         <header>
             <nav class="navbar navbar-inverse navbar-fixed-top">
                 <div class="container-fluid">
@@ -58,22 +49,6 @@
         </header>
         <div class="container-fluid">
             <div class="row">
-                <!--                <div class="col-lg-2 col-md-2 fixed">
-                                    <div class="list-group">
-                                        <a href="<?php echo site_url("dechargement"); ?>" class="left-side <?php echo $active == 'dechargement' ? 'active' : ' ' ?> list-group-item">
-                                            <h5 class="list-group-item-heading"><?php echo lang('UNLOADING'); ?></h5>
-                                        </a>
-                                        <a href="<?php echo site_url("camion"); ?>" class=" left-side <?php echo $active == 'camion' ? 'active' : ' ' ?> list-group-item">
-                                            <h5 class="list-group-item-heading"><?php echo lang('TRUCKS'); ?></h5>
-                                        </a>
-                                        <a href="<?php echo site_url("fournisseur"); ?>" class="left-side <?php echo $active == 'fournisseur' ? 'active' : ' ' ?> list-group-item">
-                                            <h5 class="list-group-item-heading"><?php echo lang('SUPPLIERS'); ?></h5>
-                                        </a>
-                                        <a href="<?php echo site_url("ville"); ?>" class="left-side <?php echo $active == 'ville' ? 'active' : ' ' ?> list-group-item">
-                                            <h5 class="list-group-item-heading"><?php echo lang('CITIES'); ?></h5>
-                                        </a>
-                                    </div>
-                                </div>-->
                 <?php
                 if (isset($_SESSION['admin']))
                 {
@@ -82,7 +57,7 @@
                         <div class="content clearfix">
                             <!--<div class="title"><h3><?php echo $title; ?></h3></div>-->
                             <p>
-                                <button type="button" href="<?php echo $form_link; ?>" data-toggle="modal" data-target="#form-content" class="btn btn-primary btn-large"><?php echo lang('ADD'); ?></button>
+                                <a href="<?php echo $form_link; ?>" data-toggle="modal" data-target="#form-content" class="btn btn-primary btn-large"><?php echo lang('ADD'); ?></a>
                             </p>
                             <div class="msg <?php echo!$error ? 'success' : 'alert-danger fade in'; ?>">
                                 <center><?php echo urldecode($msg); ?></center>
