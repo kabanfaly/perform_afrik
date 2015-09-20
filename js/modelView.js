d1fd61b2b7dbdcd5d5161dc29010425367ccec53
KABA N'faly <nfalykaba@gmail.com>
2015-09-20 19:54:12 +0200

    styling export buttons

--- js/modelView.js
+++ js/modelView.js
@@ -1,4 +1,4 @@
-define(["jquery", "jquery-ui", "datetimepicker", "datatables", 
+define(["jquery", "jquery-ui", "datetimepicker", "datatables",
     "datatables.buttons", "datatables.buttons.flash", "datatables.buttons.html5", "datatables.buttons.print",
     "bootstrap", "datatables.bootstrap"], function () {
     $(function () {
@@ -6,13 +6,16 @@ define(["jquery", "jquery-ui", "datetimepicker", "datatables",
         $('#tableContent').dataTable({
             "bProcessing": true,
             "language": {
+                buttons: {
+                    print: 'Imprimer'
+                },
                 //baseUrl is define in application/views/templates/footer.php 
                 "url": baseUrl + "assets/datatables-plugins/i18n/French.lang"
             },
             dom: 'Bfrtip',
             buttons: [
-                'csv', 'pdf', 'print'
-            ]
+                'excel', 'pdf', 'print'
+            ],
         });
     });
 });
